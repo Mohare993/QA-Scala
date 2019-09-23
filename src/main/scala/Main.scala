2dbd512d5128d1148268a0a2ffaf6ebdc5065187
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-09-23 15:58:57 +0100

    main added

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,20 @@
+
+class Main extends App{
+
+  def newName() ={
+    "hello"
+  }
+
+  def newName2() ={
+    "hello"
+  }
+
+  def newName3() ={
+    "hello"
+  }
+
+  def newName4() ={
+    "hello"
+  }
+
+}
