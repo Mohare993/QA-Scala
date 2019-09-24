6f6e5d62e335e945865aa9d9d78ba870e9632f53
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-09-24 13:04:00 +0100

    Conditionals added

--- src/main/scala/Day1/Basic.scala
+++ src/main/scala/Day1/Basic.scala
@@ -10,7 +10,7 @@ object Basic extends App {
   println(helloWorld)
 
   // Parameters
-  def printWord(word: String) = println(word)
+  def printWord(word: String): Unit = println(word)
   printWord("test")
 
   // Return types
@@ -18,21 +18,17 @@ object Basic extends App {
   println(printHelloWorld())
 
   // Type Inference
-  def returnAnyType(a: Any) = println(a)
+  def returnAnyType(a: Any): Unit = println(a)
   returnAnyType(2.003)
   returnAnyType(23)
   returnAnyType(true)
   returnAnyType("String")
 
-
   // Strings
-  def stringPrinting(str: String, num: Int) =
+  def stringPrinting(str: String, num: Int): Unit =
     println(str.takeRight(num))
 
   stringPrinting("Hello",3)
-
-
-
 
   // Strings 2
   def charReplace(str1: String, str2: String, char1: Char, char2: Char): Unit ={
@@ -42,15 +38,34 @@ object Basic extends App {
 
   charReplace("Ha", "llo", 'a', 'e')
 
-
   // Operators
-  def add(a: Int, b: Int) = a+b
+  def add(a: Int, b: Int): Int = a+b
 
   println(add(3,4))
 
   // Conditionals
+  def addConditional(a: Int, b: Int, c: Boolean): Unit = {
+    if (c) {
+      println(a+b)
+    } else println(a*b)
+  }
 
+  addConditional(2,4,true)
+  addConditional(2,4,false)
 
+  // Conditionals2
+  def conditional2(a: Int, b: Int): Unit = {
+    if (a==0 && b==0) println("Conditional2 : " + 0)
+    else if (a==0) println("Conditional2 : " + b)
+    else if (b==0) println("Conditional2 : " + a)
+    else println("Conditional2 : " + (a+b))
+  }
 
+  conditional2(0,2)
+  conditional2(4,0)
+  conditional2(5,6)
+  conditional2(0,0)
+
+  // Iteration
 
 }
