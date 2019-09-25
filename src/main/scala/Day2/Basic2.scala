67b579b6e8aac67f74347368c7ca7d31a3a272e4
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-09-25 11:03:40 +0100

    pattern match added

--- src/main/scala/Day2/Basic2.scala
+++ src/main/scala/Day2/Basic2.scala
@@ -15,12 +15,16 @@ object Basic2 extends App {
   case class Nums(num1: Int, num2: Int)
 
   def patternMatch2(nums: Any): Unit = nums match {
-
+    case Nums(0,0) => println(0)
+    case Nums(num1,0) => println(num1)
+    case Nums(0,num2) => println(num2)
+    case Nums(num1,num2) => println(num1 + num2)
   }
 
-  patternMatch2(0,2)
-  patternMatch2(4,0)
-  patternMatch2(5,6)
-  patternMatch2(0,0)
+  patternMatch2(Nums(0,2))
+  patternMatch2(Nums(4,0))
+  patternMatch2(Nums(5,6))
+  patternMatch2(Nums(0,0))
+
 
 }
