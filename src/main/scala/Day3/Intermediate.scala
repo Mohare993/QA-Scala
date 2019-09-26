6d0bc75e6d9eb1af0415ae920be58e9bffd4fae5
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-09-26 09:42:45 +0100

    Functional1 added

--- src/main/scala/Day3/Intermediate.scala
+++ src/main/scala/Day3/Intermediate.scala
@@ -1,6 +1,22 @@
 package Day3
 
+import java.util.TimeZone
+
 object Intermediate extends App {
+
+  // Functional 1
+  val timeZones: Unit = java.util.TimeZone.getAvailableIDs().map(_.split("/"))
+    .filter(zone => zone.length != 1).map(_.last)
+    .grouped(10)
+    .foreach(zone => print(zone(1) + ", "))
+
+  // Blackjack
+  case class
+
+  def blackjack(num1: Int, num2: Int) =
+
+
 
 
 }
+
