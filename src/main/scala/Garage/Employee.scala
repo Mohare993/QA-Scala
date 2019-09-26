bb0bd79cf1fac990a3c4c1b3d19b2e9f417ecbce
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-09-26 16:43:15 +0100

    garage updated

--- src/main/scala/Garage/Employee.scala
+++ src/main/scala/Garage/Employee.scala
@@ -1,5 +1,5 @@
 package Garage
 
-class Employee(fullName: String, employeeID: Int) extends Person(fullName: String) {
+class Employee(fullName: String, employeeID: Int, var canWork: Boolean) extends Person(fullName: String) {
   override def toString() = s"Employee Name : $fullName, ID : $employeeID)"
 }
