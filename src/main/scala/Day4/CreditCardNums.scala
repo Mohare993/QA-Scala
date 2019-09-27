b2aa7b7af286fa87c0233f3d2271df0a2a299f15
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-09-27 14:39:35 +0100

    identify second part added

--- src/main/scala/Day4/CreditCardNums.scala
+++ src/main/scala/Day4/CreditCardNums.scala
@@ -2,21 +2,39 @@ package Day4
 
 object CreditCardNums extends App {
 
-    def identityCheck(number:Long):Any={
-      val cardNumber = number.toString.toCharArray.map(_.toString.toLong).toList.reverse
-      val newCardNumber = cardNumber.zipWithIndex.map{
-        case (number, index) =>
-          if (index % 2 != 0){
-            val doubleNumber = number * 2
-            if (doubleNumber > 9) doubleNumber - 9 else doubleNumber
-          }
-          else number
-      }.sum
-        if (newCardNumber % 10 == 0) println("Number is Valid")
-        else println("Number is invalid")
+  def identityCheck(number: Long): Boolean = {
+    val cardNumber = number.toString.toCharArray.map(_.toString.toLong).toList.reverse
+    val newCardNumber = cardNumber.zipWithIndex.map {
+      case (number, index) =>
+        if (index % 2 != 0) {
+          val doubleNumber = number * 2
+          if (doubleNumber > 9) doubleNumber - 9 else doubleNumber
+        }
+        else number
+    }.sum
+    if (newCardNumber % 10 == 0) {
+      println("Number is Valid")
+      true
     }
-  identityCheck(49927398716L)
-  identityCheck(1112324255)
+    else {
+      println("Number is Invalid")
+      false
+    }
   }
+
+  identityCheck(49927398716L)
+  identityCheck(1112324251L)
+
+  def makeValid(number: Long): Unit = {
+    var num = number
+    while (identityCheck(num) == false) {
+      num += 1
+    }
+    println(num)
+  }
+
+  makeValid(1112324251L)
+
+}
 
 
--- src/main/scala/Day4/LumberJack.scala
+++ src/main/scala/Day4/LumberJack.scala
@@ -2,6 +2,9 @@ package Day4
 
 object LumberJack extends App {
 
+  val numberOfLogs = 7
+  val currentLogPile = ((1,1,1), (2,1,3), (1,4,1))
+
 //  val grid = getStorageGrid()
 
   def getStorageGrid(size: Int): Unit = {
