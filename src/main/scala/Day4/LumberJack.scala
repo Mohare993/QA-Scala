a8748c8478e0d2482a2a013ccae4c8b90a39484b
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-09-27 15:07:05 +0100

    lumberjack storage grid added

--- src/main/scala/Day4/LumberJack.scala
+++ src/main/scala/Day4/LumberJack.scala
@@ -1,16 +1,23 @@
 package Day4
 
 object LumberJack extends App {
+  var gridSize: Int = 3
+  var numberOfLogs = 7
+  var logPile: Seq[Int] = Seq(1, 1, 1, 2, 1, 3, 1, 4, 1)
+  var count = 0
 
-  val numberOfLogs = 7
-  val currentLogPile = ((1,1,1), (2,1,3), (1,4,1))
-
-//  val grid = getStorageGrid()
-
-  def getStorageGrid(size: Int): Unit = {
-    var grid = Array.ofDim[Int](size, size)
-    print(grid.map(_.mkString).mkString("\n"))
-    return grid
-
+  def getStorageGrid(): Unit = {
+    if (count % gridSize == 0) {
+      print("\n")
+    }
+    print(logPile(count))
+    count += 1
+    if (count == logPile.size) {}
+    else getStorageGrid()
   }
+
+  getStorageGrid()
+
 }
+
+ 
