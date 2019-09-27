e31fbaebc90098fad915b1329037e0e040352d5d
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-09-27 15:22:41 +0100

    storagecalc added

--- src/main/scala/Day4/LumberJack.scala
+++ src/main/scala/Day4/LumberJack.scala
@@ -3,8 +3,12 @@ package Day4
 object LumberJack extends App {
   var gridSize: Int = 3
   var numberOfLogs = 7
-  var logPile: Seq[Int] = Seq(1, 1, 1, 2, 1, 3, 1, 4, 1)
+  var logPile: List[Int] = List(1, 1, 1, 2, 1, 3, 1, 4, 1)
   var count = 0
+
+  def lumberjack (gridSize: Int, numberOfLogs: Int, logPile: List[Int]): Unit = {
+
+  }
 
   def getStorageGrid(): Unit = {
     if (count % gridSize == 0) {
@@ -16,7 +20,23 @@ object LumberJack extends App {
     else getStorageGrid()
   }
 
+  def storageCalculator(): Unit = {
+    if (numberOfLogs > 0) {
+      if (count == logPile.size) {
+        count = 0 }
+      if (logPile(count) == logPile.min) {
+        logPile = logPile.updated(count, logPile(count) + 1)
+        numberOfLogs -= 1 }
+      count += 1
+      storageCalculator()
+    } else {
+      count = 0
+      getStorageGrid()
+    }
+  }
+
   getStorageGrid()
+  storageCalculator()
 
 }
 
