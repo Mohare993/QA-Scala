eb9573e20e3758d86d282bb886b89ca83e5ddbb5
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-10-01 11:54:30 +0100

    Subsequence Updated

--- src/main/scala/Day5/Subsequence.scala
+++ src/main/scala/Day5/Subsequence.scala
@@ -2,21 +2,19 @@ package Day5
 
 import scala.math.max
 
-object Subseq extends App {
+object Subsequence extends App {
 
   def getAllSubstrings(str: String): Set[String] = {
     str.inits.flatMap(_.tails).toSet
   }
 
-  def longestCommonSubstring(str1: String, str2: String): String = {
+  def longestCommonSubsequence(str1: String, str2: String): String = {
     val str1Substrings = getAllSubstrings(str1)
     val str2Substrings = getAllSubstrings(str2)
-    println(str1Substrings)
-    println(str2Substrings)
 
     str1Substrings.intersect(str2Substrings).maxBy(_.length)
   }
 
-  println(longestCommonSubstring("ABSFSDFG", "ABCF"))
+  println(longestCommonSubsequence("ABSFSDFG", "ABCF"))
 }
 
