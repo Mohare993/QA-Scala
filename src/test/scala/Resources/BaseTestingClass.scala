7edbe38255042aa8df0941d57e7692c3a8c28963
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-10-01 10:26:20 +0100

    {} removed

--- src/test/scala/Resources/BaseTestingClass.scala
+++ src/test/scala/Resources/BaseTestingClass.scala
@@ -4,6 +4,4 @@ import org.scalatest.{FlatSpec, Inside, Inspectors, Matchers, OptionValues}
 import org.scalatestplus.mockito.MockitoSugar
 
 abstract class BaseTestingClass extends FlatSpec with Matchers with OptionValues
-  with Inside with Inspectors with MockitoSugar {
-
-}
+  with Inside with Inspectors with MockitoSugar
