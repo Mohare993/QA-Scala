9eca437815c32145d98b33f8758363ff93a04521
mohareqa <Matthew.O'Hare@academytrainee.com>
2019-10-02 09:10:45 +0100

    to doc added

--- src/main/scala/Garage/Customer.scala
+++ src/main/scala/Garage/Customer.scala
@@ -1,5 +1,12 @@
 package Garage
 
+import org.mongodb.scala.bson.collection.immutable.Document
+
 class Customer (fullName: String, customerID: Int) extends  Person (fullName) {
   override def toString() = s"Customer Name : $fullName, ID : $customerID"
+
+  def toDoc (fullName: String, customerID: Int): Document = Document(
+    "_id" -> customerID,
+    "fullName" -> fullName
+  )
 }
